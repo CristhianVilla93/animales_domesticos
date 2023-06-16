c00890cad7350b1d6f9d9fdf95d33463824f6c26
Cristhian Villa <crvm1993@gmail.com>
2023-06-16 18:36:06 -0500

    11avo comentario

--- src/App.jsx
+++ src/App.jsx
@@ -14,37 +14,37 @@ function App() {
         </div>
         <div className="trajetas">
           <Pagina
-            src={"src/imagenes/caballos.jpg"}
+            src={"public/imagenes/caballos.jpg"}
             animal={"Caballo"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
           <Pagina
-            src={"src/imagenes/cat.jpg"}
+            src={"public/imagenes/cat.jpg"}
             animal={"Cat"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
           <Pagina
-            src={"src/imagenes/cuy.jpg"}
+            src={"public/imagenes/cuy.jpg"}
             animal={"Cuy"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
           <Pagina
-            src={"src/imagenes/dog.jpg"}
+            src={"public/imagenes/dog.jpg"}
             animal={"Dog"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
           <Pagina
-            src={"src/imagenes/goat.jpg"}
+            src={"public/imagenes/goat.jpg"}
             animal={"Goat"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
           <Pagina
-            src={"src/imagenes/loro.jpg"}
+            src={"public/imagenes/loro.jpg"}
             animal={"Loro"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
           <Pagina
-            src={"src/imagenes/vaca.jpg"}
+            src={"public/imagenes/vaca.jpg"}
             animal={"Vaca"}
             descripcion={"Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"}
           />
