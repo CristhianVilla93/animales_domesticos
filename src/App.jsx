90a8b8d23466af27448cbcc5144c013913a08807
Cristhian Villa <crvm1993@gmail.com>
2023-06-16 17:55:33 -0500

    cuarto comentario

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,85 @@ function App() {
 
   return (
     <>
-      <Pagina
-      src={"src/imagenes/caballos.jpg"}
-      animal={""}
-      descripcion={""}
-      />
+      <div className="contenedor">
+        <div className="titulo">
+          <h1>
+            <b>ANIMALES DOMESTICOS</b>
+          </h1>
+        </div>
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+        <Pagina
+          src={"src/imagenes/caballos.jpg"}
+          animal={"Caballo"}
+          descripcion={
+            "Otro de los animales domésticos más significativos de nuestra historia, asociado a la fuerza, la velocidad y el espíritu salvaje, sirvió de transporte al humano durante milenios, ya fuera montándolo directamente o usándolo como tracción de diversos vehículos a ruedas (carretas, carrozas, etc)"
+          }
+        />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
--- src/pagina/Pagina.jsx
+++ src/pagina/Pagina.jsx
@@ -2,23 +2,16 @@
 export default function Pagina(props) {
   return (
     <>
-      <div className="contenedor">
-        <div className="titulo">
-          <h1>
-            <b>ANIMALES DOMESTICOS</b>
-          </h1>
-        </div>
-        <div className="trajetas">
-          <div className="tarjet">
-            <div>
-              <img src={props.src} alt="" />
-            </div>
-            <div>
-              <samp>Animal:{props.animal}</samp>
-            </div>
-            <div>
-              <p>Descripcion:{props.descripcion}</p>
-            </div>
+      <div className="trajetas">
+        <div className="tarjet">
+          <div className="cuadroimg">
+            <img className="img" src={props.src} alt="" />
+          </div>
+          <div>
+            <samp>Animal:{props.animal}</samp>
+          </div>
+          <div>
+            <p>Descripcion:{props.descripcion}</p>
           </div>
         </div>
       </div>
